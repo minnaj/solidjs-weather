6c4e88ff5bd5ef80464da378605976a2e70d0ef6
Minna Jarsma <minna.jarsma@gmail.com>
2023-12-13 15:24:04 +0200

    Display current weather in location route

--- src/pages/Location.tsx
+++ src/pages/Location.tsx
@@ -1,14 +1,17 @@
+import { Show, createResource } from "solid-js";
 import { useParams } from "@solidjs/router";
-import Card from "../components/Card";
+import getForecast from "../api/getForecast";
+import WeatherCard from "../components/WeatherCard";
 
 function Location() {
   const params = useParams();
+  const [weatherData] = createResource(params.id, getForecast);
 
   return (
     <div class="container max-w-3xl mx-auto p-4 flex flex-col gap-4">
-      <Card title="Parameters">
-        <div>{`ID: ${params.id}`}</div>
-      </Card>
+      <Show when={weatherData()}>
+        <WeatherCard data={weatherData()!} />
+      </Show>
     </div>
   );
 }
